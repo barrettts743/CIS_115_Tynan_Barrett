5e9220b53e4bb340b8395dc4fb4ffe70de8e3fc8
SANDHILLS\LH218-25 <jad.talbert@gmail.com>
2025-08-27 10:31:54 -0400

    sum number changes

---
+++ sumNumbers.py
@@ -0,0 +1,7 @@
+#this program sums two number
+
+num1 = input("Enter first numbner: ")
+num2 = input("Enter second number: ")
+sum = float(num1) + float(num2)
+
+print("The sum of {0} and {1} is {2}".format(num1,num2,sum))
