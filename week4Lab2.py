6570ce32313ae042d3e91c9a946d6002ec690da4
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-09-09 09:43:52 -0400

    new_channges

---
+++ week4Lab2.py
@@ -0,0 +1,17 @@
+#this program is to find total
+#of integer value
+num1 = -8
+num2 = -15
+
+sum = int(num1) - int(num2)
+print(sum)
+
+#find if sum is less than 0
+if (sum) > 0:
+    print('####################################')
+    print('Invalid! The value is less than zero')
+    print('####################################')
+
+else:
+    print('The values entered were valid integers.')
+    
