82868c6440d1a18c4743c655f26824908907fdaa
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-09-11 09:38:24 -0400

    newcode

---
+++ week4Lab4.py
@@ -0,0 +1,7 @@
+#grab 2 numbers from user
+num1 = float(input('What is your first number: '))
+num2 = float(input('What is your second number: '))
+
+#find total value through division
+total = (num1)//(num2)
+print(total)
