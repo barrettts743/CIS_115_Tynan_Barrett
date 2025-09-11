15eeef65e0e3c90647e99045bd57f6b30187aaf4
SANDHILLS\lh178-18 <jad.talbert@gmail.com>
2025-09-11 09:47:38 -0400

    newcode

---
+++ week4Lab5.py
@@ -0,0 +1,9 @@
+#Grab user's first and last names.
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+#Form name with space inbetween
+whole_name = first_name + ' ' + last_name
+
+#Show users full name
+print('My full name is ' + whole_name)
