6557467b4d0978dde6a33b9547da1e8ea156a3c8
barrettts743 <barrettts743@mail.sandhills.edu>
2025-09-11 19:18:00 -0400

    changes

--- week4Lab6.py
+++ week4Lab6.py
@@ -10,3 +10,6 @@ full_address = (f'{address}\n{city}, {state}\n{zip_code}')
 
 #display the address to the user.
 print(f'The address you entered is: \n{full_address}')
+
+#this is the end of the program.
+input('Press Enter to close...')
