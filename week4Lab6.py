c26f2e67181c16dd086c253d2cdbf269459a0810
barrettts743 <barrettts743@mail.sandhills.edu>
2025-09-11 17:30:10 -0400

    update

--- week4Lab6.py
+++ week4Lab6.py
@@ -4,7 +4,9 @@ address = input('Enter your Street Address: ')
 city = input('Enter your city: ')
 state = input('Enter your state: ')
 zip_code = input('Enter your zip code: ')
+
 #combine the address in a string variable.
 full_address = (f'{address}\n{city}, {state}\n{zip_code}')
+
 #display the address to the user.
 print(f'The address you entered is: \n{full_address}')
