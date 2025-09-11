cd6bc9bb74c6825eed3bdaae0a6ae0610b51d024
barrettts743 <barrettts743@mail.sandhills.edu>
2025-09-11 17:11:55 -0400

    newcoding

--- week4Lab6.py
+++ week4Lab6.py
@@ -1,2 +1,10 @@
-#this program will print a fake address to user.
-print('4444 Ballack Rd\nFredrick, VA\n36150')
+#this program will print an address to the user.
+#Grab an address from the user.
+address = input('Enter your Street Address: ')
+city = input('Enter your city: ')
+state = input('Enter your state: ')
+zip_code = input('Enter your zip code: ')
+#combine the address in a string variable.
+full_address = (f'{address}\n{city}, {state}\n{zip_code}')
+#display the address to the user.
+print(f'The address you entered is: \n{full_address}')
