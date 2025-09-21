35689406fc374398468afd167b3f5556c2d90932
barrettts743 <barrettts743@mail.sandhills.edu>
2025-09-21 18:41:50 -0400

    newCode

---
+++ loopOverString.py
@@ -0,0 +1,7 @@
+#this program takes a variable and uses a for loop to loop over each character
+#assign variable
+myVar = 'Do or do not, there is no try.'
+
+for num in range(myVar):
+    print(myVar[num])
+    
---
+++ myFirstForLoop.py
@@ -0,0 +1,6 @@
+#this program uses a for loop that allows the user to enter any number of iterations
+#grab user input
+num = int(input("Enter your number of iterations you would like to do: "))
+
+for num in range(num):
+    print("This is iteration number", num + 1)
