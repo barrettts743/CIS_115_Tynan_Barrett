63ea1fa57f30b4f2c9b5c6ef23759f08d84329d7
barrettts743 <barrettts743@mail.sandhills.edu>
2025-09-27 17:06:57 -0400

    newCodes

---
+++ messageVoid.py
@@ -0,0 +1,7 @@
+#this program will implement a user-defined function
+#that takes no arguments and returns a void
+def print_message():
+    print('Hello World!')
+
+#call the function
+print_message()
