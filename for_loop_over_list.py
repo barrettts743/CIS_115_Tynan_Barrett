03928c7b4c37c72c28bbf9359682ac6a2e5543a7
barrettts743 <barrettts743@mail.sandhills.edu>
2025-09-30 11:53:38 -0400

    newcodes

---
+++ for_loop_over_list.py
@@ -0,0 +1,11 @@
+#this program will use a user defined function to loop over
+#a list and print the loop variable
+#define user function
+def getMyList():
+    list = ['10, 20, 30, 40, 50, 60'] * 6
+    return list
+
+#assign list to variable
+myList = getMyList()
+for numbers in myList:
+    print(myList[-6])
