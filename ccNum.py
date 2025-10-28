22dd65bfbbe8f04ce324176f5b9462594ec41724
barrettts743 <barrettts743@mail.sandhills.edu>
2025-10-28 09:34:50 -0400

    start

---
+++ ccNum.py
@@ -0,0 +1,11 @@
+#this program will use the mod 10 function to validate a credit card number recieved from the user
+#create the function to define the credit card validation
+def validateCreditCard(ccNum):
+    ccNum = input('Enter your credit card number: ')
+
+
+
+
+
+
+
