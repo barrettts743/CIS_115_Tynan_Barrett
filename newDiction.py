4ee15c22a7d46153f538182120ff9b9b10f760f2
barrettts743 <barrettts743@mail.sandhills.edu>
2025-10-31 13:28:37 -0400

    save

--- newDiction.py
+++ newDiction.py
@@ -1,14 +1,14 @@
 #create a dictionary that contains at least 3 objects
 names_addresses = [{
-    "firstName": "John",
-    "lastName": "Doe",
-    "city": "New York",
-    "state": "NY",
+    "firstName": "Johnny",
+    "lastName": "Holler",
+    "city": "Vancouver",
+    "state": "BC",
     "zipCode": "10001",
     "phoneNumber": "123-456-7890"
     },
     {
-    "firstName2": "Jane",
+    "firstName2": "Bobby",
     "lastName2": "Smith",
     "city2": "Los Angeles",
     "state2": "CA",
@@ -16,23 +16,19 @@ names_addresses = [{
     "phoneNumber2": "987-654-3210"
     },
     {
-    "firstName3": "Alice",
-    "lastName3": "Johnson",
-    "city3": "Chicago",
-    "state3": "IL",
+    "firstName3": "Clayton",
+    "lastName3": "Prince",
+    "city3": "Des Moines",
+    "state3": "IA",
     "zipCode3": "60601",
     "phoneNumber3": "555-123-4567"
     }] 
 
-#create a function for user to input new entries
-def print_dictionary():
-    "firstName" = input("Enter First Name: ")
-    'lastName' = input("Enter Last Name: ")
-    'city' = input("Enter City: ")
-    'state' = input("Enter State: ")
-    'zipCode' = input("Enter Zip Code: ")
-    'phoneNumber' = input("Enter Phone Number: ")
-    
+#create user defined function
+def print_dictionary(names_addresses):
+    for entry in names_addresses:
+        print(entry)
+
 
 #print the dictionary
 print_dictionary(names_addresses)
