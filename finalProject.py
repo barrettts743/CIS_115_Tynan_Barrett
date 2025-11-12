758d6e8359917edde8e8597493b469f071d3c660
barrettts743 <barrettts743@mail.sandhills.edu>
2025-11-12 10:52:54 -0500

    update

--- finalProject.py
+++ finalProject.py
@@ -21,9 +21,13 @@ for product_id, details in shopper_catalog.items():
     print(f"{product_id} | {details}")
 print()
 print("-" * 40)
+#create an empty dictionary to hold shoppers items the wish to purchase
+shopping_cart = {}
+
 #prompt shopper to select item by product ID and the quantity desired
-selected_product = int(input("Enter the Product ID of the item you wish to purchase: "))
-selected_quantity = int(input("Enter the quantity you wish to purchase: "))
+selected_product = int(input(f"Enter the Product ID of the item you wish to purchase: "))
+selected_quantity = int(input(f'Enter the quantity you would like to purchase: '))
+
 #confirm quantity is available in inventory
 available_qoh = (list(inventory_data[selected_product - 1])[4].split(": ")[1])
 print("-" * 40)
@@ -31,18 +35,28 @@ print("-" * 40)
 print(f"You have selected {selected_quantity} of {shopper_catalog[selected_product]}.")
 print("-" * 40)
 
-#prompt shopper to confirm purchase
-def confirm_purchase():
-    confirm_purchase = input("Do you wish to confirm your purchase? (yes/no): ").strip().lower()
-    if confirm_purchase != 'yes':
-        print("Purchase cancelled.")
-        input("Enter another item you wish to purchase.")
-        return(confirm_purchase)
-    else:
-        print("Purchase confirmed.")
-        print("Please proceed to checkout.")
-        print("-" * 40)
-confirm_purchase()
+#ask user if they would like to add anymore items to their shopping cart
+def add_items():
+    confirm_purchase = input("Do you wish to add another item to your cart (y or n)? ")
+    if confirm_purchase == 'y':
+        input("Enter another product id of the item you wish to purchase: ")
+        print(f'You have add {shopper_catalog[selected_product]} to cart')
+        #check to make sure item isnt in cart already
+        if product_id == selected_product:
+          print('Item is already in your cart, Would you like to increase the quantity? ')
+        else:
+            print('Item added to cart.')
+    if confirm_purchase == 'n':
+        print("Proceed with checkout.")
+add_items()
+#calculate total cost of shopper selection
+item_price_str = shopper_catalog[selected_product].split(" - $")[1]
+item_price = float(item_price_str)
+total_cost = item_price * selected_quantity
+print(f"Your total cost is: ${total_cost:.2f}")
+print("-" * 40)
+
+
 #ask for shopper billing/shipping information
 print("Please enter your billing and shipping information.")
 first_name = input("First Name: ")
@@ -97,12 +111,9 @@ cvv = input("CVV: ")
 #confirm purchase to user
 print("Payment was successful.")
 #create invoice receipt for customer
-Print
-
-
-#calculate total cost of shopper selection
-item_price_str = shopper_catalog[selected_product].split(" - $")[1]
-item_price = float(item_price_str)
-total_cost = item_price * selected_quantity
-print(f"Your total cost is: ${total_cost:.2f}")
-print("-" * 40)
+def invoice_receipt():
+    print('-'*40)
+    print({shopping_cart})
+    print(total_cost)
+    print('-'*40)
+invoice_receipt()
