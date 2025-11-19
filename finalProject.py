898abf92d52fa894bbd5076f325ae1fdb7444d41
barrettts743 <barrettts743@mail.sandhills.edu>
2025-11-19 11:00:38 -0500

    updates

--- finalProject.py
+++ finalProject.py
@@ -23,13 +23,14 @@ for product_id, details in shopper_catalog.items():
 print()
 print("-" * 40)
 
-#create an empty dictionary to hold shoppers items the wish to purchase
-shopping_cart = {}
+
 
 
 #prompt shopper to select item by product ID and the quantity desired
 selected_product = int(input(f"Enter the Product ID of the item you wish to purchase: "))
 selected_quantity = int(input(f'Enter the quantity you would like to purchase: '))
+#create an empty dictionary to hold shoppers items the wish to purchase
+shopping_cart = {selected_product, selected_quantity}
 
 #confirm quantity is available in inventory
 available_qoh = (list(inventory_data[selected_product - 1])[4].split(": "))
@@ -39,10 +40,11 @@ print(f"You have selected {selected_quantity} of {shopper_catalog[selected_produ
 print("-" * 40)
 
 #ask user if they would like to add anymore items to their shopping cart
-input = ('Would you like to add another item (y or n)?')
+
 def added_items():
+    added_items = input('Would you like to add another item (y or n)?: ')
     if added_items == 'y':
-        print('What item would you like to add?')
+        print(input('What item would you like to add?: '))
         #check to see if item was already in cart
         if added_items == selected_product:
             print('Item is already in cart')
@@ -50,20 +52,32 @@ def added_items():
             print('Item was added to cart.')
     if added_items == 'n':
         print('Proceed to checkout')
-added_items()
-print(f'You have added {shopper_catalog[selected_product]} to cart')
+
+#call the added items function
+if added_items() == shopper_catalog:
+    print(f'You have added {shopper_catalog[selected_product]} to cart')
     
 
 #calculate total cost of shopper selection
-item_price_str = shopper_catalog[selected_product].split(" - $")[1]
-item_price = float(item_price_str)
-total_cost = item_price * selected_quantity
-print(f"Your total cost is: ${total_cost:.2f}")
-print("-" * 40)
+def total_cost():
+    item_price_str = shopper_catalog[selected_product].split(" - $")
+    item_price = float(item_price_str)
+    total_cost = item_price * selected_quantity
+    print(f"Your total cost is: ${total_cost:.2f}")
+    print("-" * 40)
 
 #ask for shopper billing/shipping information
-shopper_info = {}
-print("Please enter your billing and shipping information.")
+#dictionary for shopper information
+shopper_info = {
+        'first name': ('first_name'),
+        "last name": ('last_name'),
+        "address": ('address'),
+        "city": ('city'),
+        "state": ('state'),
+        "zip_code": ('zip_code')
+}
+
+print("Please enter your billing and shipping information.")    
 first_name = input("First Name: ")
 last_name = input("Last Name: ")
 address = input("Address: ")
@@ -71,19 +85,9 @@ city = input("City: ")
 state = input("State: ")
 zip_code = input("Zip Code: ")
 print("-" * 40)
-#dictionary for shopper information
-shopper_info = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "address": address,
-        "city": city,
-        "state": state,
-        "zip_code": zip_code
-}
 
 #proceed to have shopper enter payment details
 print("Please enter your payment details.")
-
 #execute mod 10 check for cc number validation
 def validateCreditCard(ccNum):
     ccNum = input("Enter your credit card number: ")
